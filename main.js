4adeb4c8cd0ef84bcd51ed623f6326e39c803a4e
Stefano <stefano.zaccaria29@gmail.com>
2020-07-02 15:08:52 +0200

    ex complete

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ $('.header-right>a').click(function () {
 $('.hamburger-menu>a').click(function () {
     $('.hamburger-menu').hide();
 });
+
+// // ALTRA MODALITA' 
+// $('.header-right>a').click(function () {
+//     $('.hamburger-menu').fadeIn(1000);
+// });
+// $('.hamburger-menu>a').click(function () {
+//     $('.hamburger-menu').fadeOut(1200);
+// });
