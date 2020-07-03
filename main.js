cc3935f216b4daa93da17abffa84d32444a23f73
Stefano <stefano.zaccaria29@gmail.com>
2020-07-03 11:44:23 +0200

    complete

--- main.js
+++ main.js
@@ -15,12 +15,12 @@ $('.hamburger-menu>a').click(function () {
 
 // $('.header-right>a').click(function () {
 //     $('.hamburger-menu').fadeIn(1000);
-//     $('.hamburger-menu>a').mouseenter(function(){
-//         $('.hamburger-menu>a').css("color","green");  
-//     });
-//     $('.hamburger-menu>a').mouseleave(function(){
-//             $('.hamburger-menu>a').css("color","");  
-//     });
+        // $('.hamburger-menu a').mouseenter(function(){
+        // $(this).css("color","green");  
+        // });
+        // $('.hamburger-menu a').mouseleave(function(){
+        // $(this).css("color","");  
+        // });
 // });
 // $('.hamburger-menu>a').click(function () {
 //     $('.hamburger-menu').fadeOut(1200);
